10ed55c1a1bf307b11f7daf07d3b0aa7d690004f
dan1aa <burmeidan@gmail.com>
2023-09-02 13:46:18 +0300

    mobile, nobel-for-ukraine

--- js/page-global-nobel-intership.js
+++ js/page-global-nobel-intership.js
@@ -1,7 +1,38 @@
-const faqItems = document.querySelectorAll('.faq-item')
+let splide = new Splide('.splide', {
+    type: 'loop',
+    perMove: 1,
+    perPage: 2,
+    pagination: false,
+    breakpoints: {
+        1024: {
+            perPage: 1
+        }
+    }
+});
+
+splide.mount();
+
+
+const faqItems = document.querySelectorAll('.faq__item')
+const readMoreElements = document.querySelectorAll('.read__more')
+const showLessElements = document.querySelectorAll('.show__less')
 
 faqItems.forEach(faqItem => {
     faqItem.onclick = () => {
         faqItem.classList.toggle('active')
+    }
+})
+
+readMoreElements.forEach(readMoreElement => {
+    readMoreElement.onclick = function () {
+        const parent = this.parentElement;
+        parent.classList.toggle('active')
+    }
+})
+
+showLessElements.forEach(showLessElement => {
+    showLessElement.onclick = function () {
+        const parent = this.parentElement;
+        parent.classList.remove('active')
     }
 })
